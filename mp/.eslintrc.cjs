237713efff63ce135bb2c51b6c2f095e00a078aa
Cau <cau.ugolini@gmail.com>
2023-12-12 16:49:25 -0300

    doc<Jest> config

--- mp/.eslintrc.cjs
+++ mp/.eslintrc.cjs
@@ -1,15 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, "jest/globals": true, "node": true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    "plugin:jest/all",
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  settings: { react: { version: '18.2' }, },
+  plugins: ['react-refresh', "jest"],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    "jest/no-disabled-tests": "warn",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
+    "jest/prefer-to-have-length": "warn",
+    "jest/valid-expect": "error"
   },
 }
